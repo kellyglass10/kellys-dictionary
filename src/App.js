a88f10680197b54f6f4b4fac9372e9b63fcad15e
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-06-09 21:10:45 -0700

    added-dictionary-component
    added basic search form and useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by Kelly Glass. Open-sourced on Github and hosted on Netlify
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
