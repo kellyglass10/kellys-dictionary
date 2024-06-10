59dabd222fa8c2a862f6140ef0a3693b74ee1baf
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-06-09 22:41:00 -0700

    added-axios
    added an api for dictionary data

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleDictionaryResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiKey = "09ofabt96a856ac54d57c0bab329361b";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
   }
 
   function handleKeyWordChange(event) {
