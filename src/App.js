7c115ac7068e1ddf58a93d5d7c4074a425d53d37
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2024-06-13 21:42:22 -0700

    added-synonym-component
    added new component and some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import logo from "./logo.png";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">KG Dictionary</header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary />
         </main>
